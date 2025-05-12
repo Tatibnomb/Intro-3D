3e1a4b903412f032de7ff79c52c0a1eb2ca8b827
Tatibnomb <49379010@est.ort.edu.ar>
2025-05-12 10:31:50 -0300

    Boton Clonar random number terminado

---
+++ Assets/Scripts/RandomCloner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCloner : MonoBehaviour
+{
+    public int randomNumber;
+    public int min;
+    public int max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void CloneRandom()
+    {
+        randomNumber = Random.Range(min, max + 1);
+    }
+}
