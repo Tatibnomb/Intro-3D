8be8b66a6e9500d9e605b536f8999e0c6abd2491
Tatibnomb <49379010@est.ort.edu.ar>
2025-05-22 10:26:21 -0300

    Ejemplo detección de colisión terminado

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+        Destroy(gameObject);
+    }
+}
