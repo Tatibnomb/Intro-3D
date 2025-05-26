97d946dd520f069e1df18266f618c9862fe21724
Tatibnomb <49379010@est.ort.edu.ar>
2025-05-26 10:20:01 -0300

    Puntos que incrementan en UI (pantalla) terminado

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public UIManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+        score = 0;
+    }
+
+    public void AddScore()
+    {
+        score++;
+        uiManager.UpdateScore(score);
+    }
+}
